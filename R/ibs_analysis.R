98059dd6e34cf7dd5ffc39fe0b5e45c78602eff2
Bingjie-Zhu <112414597+Bingjie-Zhu@users.noreply.github.com>
2026-04-17 19:37:50 +0800

    Refactor sample_pairs indexing for clarity

--- R/ibs_analysis.R
+++ R/ibs_analysis.R
@@ -37,9 +37,8 @@ build_ibs_submatrix <- function(dist_path, id_path, sample_pairs) {
   if (is.character(sample_pairs)) {
     sample_pairs <- read.table(sample_pairs)
     }
-
-  dna_names <- as.character(sample_pairs$V2)
-  rna_names <- as.character(sample_pairs$V3)
+  dna_names <- as.character(sample_pairs[, 2])
+  rna_names <- as.character(sample_pairs[, 3])
   sub_mat <- sim_mat[dna_names, rna_names]
   return(sub_mat)
 }
