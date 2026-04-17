79364c84a4025848940c4f0de640a4ab1659a9d6
Bingjie-Zhu <112414597+Bingjie-Zhu@users.noreply.github.com>
2026-04-17 20:00:40 +0800

    Update plot_ibs_distribution.R

--- R/plot_ibs_distribution.R
+++ R/plot_ibs_distribution.R
@@ -51,7 +51,7 @@ plot_ibs_distribution <- function(sub_mat,
                         linetype = "dashed", color = "blue") +
     ggplot2::annotate("text",
                       x = bg_threshold,
-                      y = 5,
+                      y = Inf,
                       label = paste0("BG Top 1%\n(",
                                      round(bg_threshold, 4), ")"),
                       hjust = -0.05, vjust = -0.5, color = "blue", size = 3) +
